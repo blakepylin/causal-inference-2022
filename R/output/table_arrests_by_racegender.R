cd0ef95c719beea1200b98f2a48671f1ef823b5e
blakepylin <blakelin@utexas.edu>
2022-02-16 18:09:35 -0600

    changed titile name

--- R/output/table_arrests_by_racegender.R
+++ R/output/table_arrests_by_racegender.R
@@ -8,22 +8,22 @@ read_csv(here("data/NLSY97_clean.csv")) %>%
   
   # summarize arrests by race and gender
   group_by(race, gender) %>%
-  summarize(total_arrests = mean(total_arrests)) %>%
+  summarize(incarc_status = mean(incarc_status)) %>%
   
   # pivot the values from race into columns
-  pivot_wider(names_from = race, values_from = total_arrests) %>%
+  pivot_wider(names_from = race, values_from = incarc_status) %>%
   
   # rename columns using snakecase
   rename_with(to_title_case) %>%
   
   # create the kable object. Requires booktabs and float LaTeX packages
   kbl(
-    caption = "Mean arrests in 2002 by Race and Gender",
+    caption = "Average Incarceration Status in 2002 by Race and Gender",
     booktabs = TRUE,
     format = "latex",
     label = "tab:summarystats"
   ) %>%
   kable_styling(latex_options = c("striped", "HOLD_position")) %>%
   
-  write_lines(here("tables/arrests_by_racegender.tex"))
+  write_lines(here("tables/incarc_by_racegender.tex"))
   
